a4f41c0baaed94efdace12a349b80ff68a1fe7f7
Anjeh Mbah <junez24@yahoo.com>
2021-05-29 05:48:38 -0400

    added text area and trying to implement prevent default though not yet functional

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-
+//displaying day and time using moment.js
 var dayofWeek = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(dayofWeek);
+
+
+//trying to stop the text area from being a blank aftre the page is refreshed.
+$( "textarea" ).click(function( event ) {
+    event.preventDefault();
+    $( "<div>" )
+      .append( "default " + event.type + " prevented" )
+      .appendTo( "#textspace" );
+  });
