acba795793340e37be254745400186de135ef910
Anjeh Mbah <junez24@yahoo.com>
2021-06-01 05:33:50 -0400

    Redone html for better outcome

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-//displaying day and time using moment.js
+//displaying present day and time using moment.js
 var dayofWeek = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(dayofWeek);
 
 
-//trying to stop the text area from being a blank aftre the page is refreshed.
-$( "textarea" ).click(function( event ) {
-    event.preventDefault();
-    $( "<div>" )
-      .append( "default " + event.type + " prevented" )
-      .appendTo( "#textspace" );
-  });
+//Using presentHour to decide if the hour matches that displayed in header so as to apply color change
+
+var presentHour = moment().format('LT');
+
+var allhoursValue = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM'];
+var exactHour = presentHour;
+var futureHour = presentHour < allhoursValue;
+var pastHour = allhoursValue < exactHour;
+
+console.log(presentHour)
+console.log(exactHour)
+console.log(pastHour)
+
+function colorChange() {
+    if (exactHour == presentHour){
+        document.getElementsByClassName('col2').style.backgroundColor = 'red';
+    }
+    else if (pastHour < presentHour){
+        document.getElementsByClassName('col2').style.backgroundColor = 'grey';
+    }
+    else (presentHour > futureHour) 
+    {
+        document.getElementsByClassName('col2').style.backgroundColor = 'green';
+    }
+}
